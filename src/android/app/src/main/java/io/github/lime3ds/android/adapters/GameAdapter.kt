b23d572cbfd1947f7064e48b307e995b3a33c08a
kleidis <167202775+kleidis@users.noreply.github.com>
2024-07-23 15:47:13 +0200

    Swap gameid and filename in about game menu

--- src/android/app/src/main/java/io/github/lime3ds/android/adapters/GameAdapter.kt
+++ src/android/app/src/main/java/io/github/lime3ds/android/adapters/GameAdapter.kt
@@ -203,8 +203,8 @@ class GameAdapter(private val activity: AppCompatActivity, private val inflater:
         bottomSheetDialog.setContentView(bottomSheetView)
 
         bottomSheetView.findViewById<TextView>(R.id.about_game_title).text = game.title
-        bottomSheetView.findViewById<TextView>(R.id.about_game_filename).text = game.filename
         bottomSheetView.findViewById<TextView>(R.id.about_game_id).text = String.format("%016X", game.titleId)
+        bottomSheetView.findViewById<TextView>(R.id.about_game_filename).text = game.filename
         GameIconUtils.loadGameIcon(activity, game, bottomSheetView.findViewById(R.id.game_icon))
 
         bottomSheetView.findViewById<MaterialButton>(R.id.about_game_play).setOnClickListener {
