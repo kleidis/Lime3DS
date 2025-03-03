0f5e274136e329863562510bf47c34c42d5f835b
Charles Lombardo <clombardo169@gmail.com>
2025-03-03 16:48:31 +0100

    Add setVisible function to manage view visibility

--- src/android/app/src/main/java/org/citra/citra_emu/utils/ViewUtils.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/utils/ViewUtils.kt
@@ -19,6 +19,24 @@ object ViewUtils {
         }.start()
     }
 
+    /**
+     * Shows or hides a view.
+     * @param visible Whether a view will be made View.VISIBLE or View.INVISIBLE/GONE.
+     * @param gone Optional parameter for hiding a view. Uses View.GONE if true and View.INVISIBLE otherwise.
+     */
+    fun View.setVisible(visible: Boolean, gone: Boolean = true) {
+        visibility = if (visible) {
+            View.VISIBLE
+        } else {
+            if (gone) {
+                View.GONE
+            } else {
+                View.INVISIBLE
+            }
+        }
+    }
+
+
     fun hideView(view: View, length: Long = 300) {
         if (view.visibility == View.INVISIBLE) {
             return
